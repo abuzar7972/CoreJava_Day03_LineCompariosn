ebed560a6d0fbca5c27fc23b6f4f261dd3ce7a04
abuzar7972 <abushaikh7972@gmail.com>
2021-06-21 15:33:15 +0530

    Line based on point using Cartesian system

--- LineComparisonProblem.java
+++ LineComparisonProblem.java
@@ -1,5 +1,11 @@
 public class LineComparisonProblem{
 	public static void main(String[] args){
-		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+		int x1=3;
+		int x2=4;
+		int y1=2;
+		int y2=7;
+		double line = ((x2-x1)*2)+((y2-y1)*2);
+		System.out.println(line*0.5);
+
 }
 }
